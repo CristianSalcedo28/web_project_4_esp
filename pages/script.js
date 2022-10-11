63b39ac4eacc56ed00d6c0221e6719ce792f1ebe
Cristian David Salcedo <cristian.salcedo@outlook.com>
2022-10-10 23:03:41 -0300

    Final

---
+++ pages/script.js
@@ -0,0 +1,31 @@
+const openFormButton = document.querySelector('.button-edit'); 
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.close-button');
+
+function toggleForm() {
+  popup.classList.toggle('popup__opened');
+}
+
+openFormButton.addEventListener('click', toggleForm);
+closeButton.addEventListener('click', toggleForm); 
+
+const formElement = document.querySelector('.popup__container')
+
+function cambiarTitulos(evt){
+  evt.preventDefault();
+  const inputName = document.querySelector('.popup__item_name').value;
+  const inputProfession = document.querySelector('.popup__item_profession').value;
+
+  const profileName = document.querySelector('.profile__name');
+  const profileProfession = document.querySelector('.profile__profession');
+  
+  profileName.textContent = inputName;
+  profileProfession.textContent = inputProfession;
+
+}
+
+const submitButton = document.querySelector('.button-submit');
+submitButton.addEventListener("click", toggleForm);
+
+document.querySelector(".popup__form").addEventListener('submit', cambiarTitulos); 
+
